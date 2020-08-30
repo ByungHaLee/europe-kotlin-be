ade0d82ae58a95c8f37bc94c1dc2ec18327ccbd2
Byung Ha Lee <byungha.lee@metronom.com>
2020-08-31 00:07:08 +0200

    Init

---
+++ src/main/kotlin/study/europe/europekotlinbe/EuropeKotlinBeApplication.kt
@@ -0,0 +1,11 @@
+package study.europe.europekotlinbe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EuropeKotlinBeApplication
+
+fun main(args: Array<String>) {
+	runApplication<EuropeKotlinBeApplication>(*args)
+}
---
+++ src/test/kotlin/study/europe/europekotlinbe/EuropeKotlinBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.europe.europekotlinbe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EuropeKotlinBeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
